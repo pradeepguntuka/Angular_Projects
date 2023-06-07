1e2118af6cf23eaf2a000eafdd8908862d9f5658
pradeepguntuka <93825284+pradeepguntuka@users.noreply.github.com>
2023-06-07 15:40:33 +0530

    Update todo.component.ts

--- Todo_component/todo.component.ts
+++ Todo_component/todo.component.ts
@@ -1,1 +1,34 @@
+import { Component, OnInit } from '@angular/core';
 
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  todoList : string []=[];
+
+  inputvalue: string="";
+
+  removetodo(name:string){
+    this.todoList=this.todoList.filter(value=> value !== name);
+  }
+
+  addtodo(){
+  if(this.inputvalue !=""){
+    this.todoList.push(this.inputvalue);
+    this.inputvalue="";
+  }
+  else{
+    alert("Please enter proper value...")
+  }
+  }
+  constructor(){ }
+
+  ngOnInit(): void {
+    this.todoList=["Task 1","Task 2"];
+  }
+
+
+}
